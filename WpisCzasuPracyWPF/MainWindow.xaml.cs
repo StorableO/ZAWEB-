d000cb3f1f7d98925f172b35915c4d440ad784b3
Denis <166916866+StorableO@users.noreply.github.com>
2025-12-10 13:27:55 +0100

    Add files via upload

---
+++ WpisCzasuPracyWPF/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+namespace Zadanie1WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<WpisCzasuPracy> list = new List<WpisCzasuPracy> ();
+        public MainWindow()
+        {
+            InitializeComponent();
+            RenderDataGrid ();
+        }
+        private void RenderDataGrid()
+        {
+            dgDane.ItemsSource = null;
+            dgDane.ItemsSource = list;
+
+            tbImie.Text = string.Empty;
+            tbGodziny.Text = string.Empty;
+            dpData.SelectedDate = DateTime.Now;
+        }
+
+        private void btndodaj_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string name = tbImie.Text.Trim().ToString();
+                DateTime dateTime = Convert.ToDateTime(dpData.SelectedDate); 
+                int hours = Convert.ToInt32(tbGodziny.Text.Trim().ToString());
+
+                WpisCzasuPracy Pracownik = new WpisCzasuPracy(name,dateTime,hours);
+                list.Add(Pracownik);
+
+                RenderDataGrid();
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+            
+        }
+
+        private void btnUsun_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var wpis = button.DataContext as WpisCzasuPracy;
+
+            if(wpis != null)
+            {
+                list.Remove(wpis);
+            }
+
+            RenderDataGrid();
+        }
+    }
+}
---
+++ WpisCzasuPracyWPF/WpisCzasuPracy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1WPF
+{
+    public class WpisCzasuPracy
+    {
+        public string Pracownik { get; set; }
+        public DateTime? Data { get; set; }
+        public int Godziny { get; set; }
+
+        public WpisCzasuPracy(string imie, DateTime data, int godziny)
+        {
+            Pracownik = imie;
+            Data = data;
+            Godziny = godziny;
+        }
+    }
+}
